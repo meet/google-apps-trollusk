ca457fd9ffe64c7c30882f1a77ecfa2784a5bff4
Eric Timmons <etimmons@mit.edu>
2013-10-06 10:28:59 -0400

    First version of pure ruby implementation using watir-webdriver

--- lib/google-apps-trollusk.rb
+++ lib/google-apps-trollusk.rb
@@ -1,4 +1,6 @@
 require 'rexml/document'
+require 'watir-webdriver'
+require 'headless'
 
 module GoogleApps
   
@@ -9,9 +11,9 @@ module GoogleApps
     end
     class Route < Struct.new(:destination, :rewrite_to, :enabled)
     end
-    
+
     @@connection_params = { }
-    
+    @@cpanel = "https://admin.google.com/meet.mit.edu/"
     # Add connection parameters.
     def self.connect_with(connection_params)
       @@connection_params.merge!(connection_params)
@@ -21,26 +23,17 @@ module GoogleApps
     def self.connect
       yield MockTrollusk.new and return if @@connection_params[:mock]
       
-      path = File.expand_path('../../', __FILE__)
-      doc = REXML::Document.new(File.new("#{path}/pom.xml"))
-      name = doc.elements['project/artifactId'].text
-      version = doc.elements['project/version'].text
       domain = @@connection_params[:domain]
       username = @@connection_params[:username]
-      jar = "target/#{name}-#{version}-jar-with-dependencies.jar"
+      password = @@connection_params[:password]
       
-      raise TrolluskError.new("Missing #{jar}") unless File.exists? "#{path}/#{jar}"
-      cmd = "java -jar '#{path}/#{jar}' '#{domain}' '#{username}'"
-      IO.popen(cmd, 'r+') do |io|
-        yield self.new(io, @@connection_params[:password])
-        io.close_write
-      end
-    end
-    
-    def self.obfuscate(password)
-      password.bytes.zip(@@connection_params[:obfuscation].bytes).map do |p, q|
-        p ^ q
-      end .map(&:chr).join
+      #raise TrolluskError.new("Missing #{jar}") unless File.exists? "#{path}/#{jar}"
+      #cmd = "java -jar '#{path}/#{jar}' '#{domain}' '#{username}'"
+      #IO.popen(cmd, 'r+') do |io|
+      #  yield self.new(io, @@connection_params[:password])
+      #  io.close_write
+      #end
+      self.new(domain, username, password)
     end
     
     def self.parse(user)
@@ -54,59 +47,152 @@ module GoogleApps
       User.new(m[1], m[2]!='-', m[3]!='-', routes)
     end
     
-    def initialize(io, password)
-      class << io
-        def gets
-          str = super
-          raise TrolluskError.new('Unexpected end of IO') unless str
-          return str
-        end
-      end
-      raise TrolluskError.new('Authentication missing') unless io.gets.strip == 'password'
-      io.puts self.class.obfuscate(password)
-      raise TrolluskError.new('Authentication failed') unless io.gets.strip == 'ok'
-      @io = io
+    def initialize(domain, username, password)
+      #@c = Connection.new(domain, username, password)
+      #@h = Headless.new
+      #@h.start
+      @b = Watir::Browser.start "https://www.google.com/a/cpanel/#{domain}"#, :chrome#, :path => '/usr/lib64/chromium-browser/chromium-browser'
+      @b.text_field(:id => 'Email').set "#{username}@#{domain}"
+      @b.text_field(:id => 'Passwd').set password
+      @b.button(:id => 'signIn').click
+      @domain = domain
+      @current_user = nil
+      #raise TrolluskError.new('Authentication missing') unless io.gets.strip == 'password'
+      #io.puts self.class.obfuscate(password)
+      #raise TrolluskError.new('Authentication failed') unless io.gets.strip == 'ok'
+      #@io = io
     end
     
-    # Get current routing settings.
+    def getBrowser
+      @b
+    end
+    
     def get(username)
-      cmd("#{username} get")
+      # Load the user's page.
+      open(username)
+      inbox = deliver_to_inbox_elt.set?
+      inherit = inherit_routes_elt.set?
+      routes = []
+      idx = 1
+      while route_enabled_elt(idx).exists?
+        enabled = route_enabled_elt(idx).set?
+        destination = route_destination_elt(idx).value
+        rewrite = route_rewrite_to_elt(idx).set?
+        routes.push(Route.new(destination, rewrite==true, enabled==true))
+        idx = idx + 1
+      end
+      User.new(username, inbox, inherit, routes)
     end
     
     # Set whether mail is delivered to the Google Apps inbox.
     def deliver_to_inbox(username, inbox)
-      cmd("#{username} inbox #{inbox}")
+      open(username)
+      deliver_to_inbox_elt.set inbox
+      save
+      get(username)
     end
     
     # Add a routing destination.
     def add(username, destination)
-      cmd("#{username} add #{destination}")
+      open(username)
+      u = get(username)
+      # Find the number of current routes:
+      idx = u.routes.length + 1
+      # Hit the button to add a new destination.
+      @b.div(:text => 'Add another destination').click
+      # Write the info:
+      route_enabled_elt(idx).set true
+      route_rewrite_to_elt(idx).set true
+      route_destination_elt(idx).set destination
+      save
+      get(username)
     end
     
     # Replace a routing destination.
     def update(username, old_destination, new_destination)
-      cmd("#{username} update #{old_destination} #{new_destination}")
+      u = get(username)
+      # Find the index of the destination
+      idx = username.routes.find_index{|r| r.destination == old_destination} + 1
+      route_destination_elt(idx).set new_destination
+      save
+      get(username)
     end
     
     # Remove a routing destination.
     def remove(username, destination)
-      cmd("#{username} remove #{destination}")
+      u = get(username)
+      # Find the index of the destination
+      idx = username.routes.find_index{|r| r.destination == old_destination} + 1
+      raise TrolluskError.new("Unimplemented")
     end
     
     # If destination_or_inbox is +'inbox'+, deliver only to inbox; otherwise,
     # deliver only to the given destination, removing any other destinations.
     def only(username, destination_or_inbox)
-      cmd("#{username} only #{destination_or_inbox}")
+      u = get(username)
+      if destination_or_inbox == 'inbox'
+        deliver_to_inbox(username, true)
+        u.routes.for_each{|r| remove(username, r.destination)}
+      end
+      raise TrolluskError.new("Unimplemented")
     end
     
+    def add_destination_elt
+      @b.div(:xpath => "//div[.='Email routing']/ancestor::tr[1]//div[.='Add another destination']")
+    end
+    
+    def save_changes_elt
+      @b.div(:xpath => "//div[contains(@class, 'pendingPanel')]//div[.='Save changes']")
+    end
+    
+    def remove_destination_elt(idx)
+      @b.div(:xpath => "//input[@name='routeDestination#{idx}']/ancestor::tr[1]//div[.='Remove']")
+    end
+
     private
-      
-      def cmd(string)
-        @io.puts(string)
-        self.class.parse(@io.gets.strip)
+      def deliver_to_inbox_elt
+        @b.checkbox(:name => 'googleAppsEmailEnabled')
       end
       
-  end
+      def inherit_routes_elt
+        @b.checkbox(:name => 'inheritRoutesEnabled')
+      end
+      
+      def route_enabled_elt(idx)
+        @b.checkbox(:name => "routeEnabled#{idx}")
+      end
+      
+      def route_rewrite_to_elt(idx)
+        @b.checkbox(:name => "routeRewriteTo#{idx}")
+      end
+      
+      def route_destination_elt(idx)
+        @b.text_field(:name => "routeDestination#{idx}")
+      end
+      
+      
+      
+      def save
+        elt = save_changes_elt
+        if elt.exists?
+          elt.click
+          true
+        else
+          false
+        end
+      end
+      
+      # Get current routing settings.
+      def open(username)
+        @b.goto "#{@@cpanel}Organization?userEmail=#{username}@#{@domain}" unless @current_user == username
+        @current_user = username
+        # Wait until all the stupid AJAX is done loading.
+        done_loading = false
+        while not deliver_to_inbox_elt.exists?
+          sleep(0.5)
+        end
+      end
+    end
   
   class MockTrollusk < Trollusk
     
